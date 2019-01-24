b0ccfd17dc6f1f8a4d2cc472530583380d3fe308
Alan <openxw@qq.com>
2019-01-24 16:21:03 +0800

    Finish user signup

---
+++ resources/views/errors/t404.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>页面出错了</title>
+    <meta http-equiv="refresh" content="10;url=//bellawb.test" />
+</head>
+
+<body>
+<a href="{{ route('home') }}"><img src="/img/404.jpeg" width="100%"></a>
+</body>
+</html>
